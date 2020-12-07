4f365f9b488d645166726b46b479c21238e49ff6
DimasVeliz <trevor90094@yandex.com>
2020-12-07 23:09:17 +0100

    Posting with Axios completed and working

--- src/components/panels/Panels.js
+++ src/components/panels/Panels.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const Panels = () => {
 
-    const [data, setData] = useState({configuration:[]});
+    const [data, setData] = useState({ configuration: [] });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,40 +21,55 @@ const Panels = () => {
 
         fetchData();
     }, [])
-    
-    
-   
+
+    const PostSelectedConf = () => {
+
+
+        const jetsonData = {
+            "cfg": "yolov3-tiny-nc_ideaBinariaRam.cfg",
+            "weight": "yolov3-tiny-nc_ideaBinariaRamProp_final.weights",
+            "labelset": "binario.names",
+            "mode": "video_file"
+        }
+
+        axios.post('http://localhost:3000/jetsonstart', jetsonData)
+            .then(response => console.log(response));
+    }
+
     if (data.configuration.length !== 0) {
         let checking = data.configuration.map((item) => (
             console.log(item)
         ));
     }
     return (
-       
 
-            <div className="bgimg">
-                <div className="row">
-                    <div className="col-md">
-                        <br />
-                        <h1 className="d-md-flex justify-content-center justify-content-md-center align-items-md-center">TEST CONTROLS</h1>
-                    </div>
+
+        <div className="bgimg">
+            <div className="row">
+                <div className="col-md">
+                    <br />
+                    <h1 className="d-md-flex justify-content-center justify-content-md-center align-items-md-center">TEST CONTROLS</h1>
                 </div>
-                <div className="row">
-                    <div className="col-md">
-                        <PanelPLC></PanelPLC>
-                    </div>
-                    <div className="col-md">
-                        <PanelJetson></PanelJetson>
-                    </div>
-                    <div className="col-md">
-                        <PanelLectura></PanelLectura>
-                    </div>
-
+            </div>
+            <div className="row">
+                <div className="col-md">
+                    <PanelPLC></PanelPLC>
+                </div>
+                <div className="col-md">
+                    <PanelJetson></PanelJetson>
+                </div>
+                <div className="col-md">
+                    <PanelLectura></PanelLectura>
                 </div>
 
             </div>
-            
-        
+            <div>
+                <button onClick={() => PostSelectedConf()}>Postea a tu Jetson</button>
+
+            </div>
+        </div>
+
+
     );
 }
 
